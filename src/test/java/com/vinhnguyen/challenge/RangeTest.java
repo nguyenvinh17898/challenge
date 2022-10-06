6b5db84ea648153bd7a34c5c4fd0b84b35f1f854
Nguyen Vinh <nguyenvinh17898@gmail.com>
2022-10-06 23:43:26 +0700

    add case test exception

--- src/test/java/com/vinhnguyen/challenge/RangeTest.java
+++ src/test/java/com/vinhnguyen/challenge/RangeTest.java
@@ -1,6 +1,7 @@
 package com.vinhnguyen.challenge;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -11,5 +12,15 @@ public class RangeTest {
 		Range validAgesForHighSchool = Range.of(16, 18);
 		assertEquals(validAgesForHighSchool.contains(5), false); // false
 		assertEquals(validAgesForHighSchool.contains(17), true); // true
+		
+		//test exception
+		try {
+			Range.of(20, 18);
+	        fail();
+	    } 
+	    catch (Exception e) {
+	        final String expected = "lowerbound must be less than upperbound";
+	        assertEquals( expected, e.getMessage());
+	    }   
 	}
 }
